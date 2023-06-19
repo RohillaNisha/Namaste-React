1a4627a594b553a92eaec81a977c4711250411a3
Nisha <nisha.rohilla@gmail.com>
2023-06-19 11:57:49 +0200

    episode - talk is cheap , lets code.

--- App.js
+++ App.js
@@ -1,6 +1,78 @@
-const parent = React.createElement("div", { id: "parent"},[
-    React.createElement("h1",{id:"first"}, "I am first sibling"),
-    React.createElement("h2", {id: "second"}, "I am second sibling"),
-]);
-        const root = ReactDOM.createRoot(document.getElementById("root"));
-        root.render(parent);
+import React from "react";
+import ReactDOM from "react-dom/client";
+import logo from "./images/logo.jpg";
+import hotel1 from "./images/hotel1.jpg";
+
+/* 
+ * Header
+   - Logo
+   - Nav items
+   - Cart
+
+ * Body
+   - Search component
+   - RestaurantContainer 
+        - RestaurantCard
+            - Image
+            - Name of Res, star rating, cuisine, delivery time
+
+ * Footer
+    - Copyright
+    - Links
+    - Address
+    - Contact
+
+*/
+
+const Header = () =>{
+    return(
+        <div className="header">
+            <div className="logo-container">
+                <img className="logo" src={logo}/>
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+const RestaurantCard = () => {
+    return(
+        <div className="res-card">
+            <img src={hotel1} className="hotel"/>
+            <h3>Meghana Foods</h3>
+            <h4> Indian Food,Asian</h4>
+            <h4>4.4 stars</h4>
+            <h4>38 minutes</h4>    
+        </div>
+    )
+}
+const Body = () => {
+    return(
+        <div className="body">
+            <div className="search">Search</div>
+            <div className="res-container" >
+            <RestaurantCard/>
+            </div>
+        </div>
+    )
+}
+
+const AppLayout = () => {
+    return (
+        <div className="app">
+         <Header/>
+         <Body/>
+        </div>
+    )
+}
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout/>);
--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # Namaste React
+
+
+# Parcel
+- Dev Build
+- Local Server
+- HMR = Hot module replacement  ( Auto refresh)
+- Uses a File Watching Algorithm which is written in C++.
+- Caching - Faster Builds.
+- Image optimization
+- Minification 
+- Bundling ( to bundle files up)
+- Compress our files
+- Consistent Hashing
+- Code Splitting 
+- Differential Bundling - support older browsers
+- Diagnostic
+- Error Handling
+- Provides way to host HTTPS
+- Tree Shaking ( algorithm , remove unused code)
+- Different build for development and production ( dev build & prod build)
+
+
+
