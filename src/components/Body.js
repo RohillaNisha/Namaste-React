e750acacb828c7ae973b01bdd7b1e0af10a90cff
Nisha <nisha.rohilla@gmail.com>
2023-06-27 16:04:11 +0200

    EPISODE 6

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,24 +1,41 @@
 import React from 'react';
 import RestaurantCard from './RestaurantCard';
 import resList from '../utils/mockData';
-import { useState } from 'react';
+import { useState , useEffect } from 'react';
 
 
 
 function Body() {
 
     const [listOfRestaurants, setListOfRestaurants] = useState(resList)
+
+    useEffect(() =>{
+        fetchData();
+    }, []);
+
+    const fetchData = async () => {
+        const data =  await fetch("https://www.swiggy.com/mapi/homepage/getCards?lat=28.7093202&lng=77.21384429999999");
+
+        const json = await data.json();
+
+        console.log(json.data.success.cards[5].gridWidget.gridElements.infoWithStyle.restaurants);
+        
+        //  console.log(json.data.success.cards[5].gridWidget.gridElements.infoWithStyle.restaurants[0].info.id);
+
+
+        setListOfRestaurants(json.data.success.cards[5].gridWidget.gridElements.infoWithStyle.restaurants);
+    }
         return(
             <div className="body">
                 <div className="filter">
                     <button className='filter-btn' onClick={()=>{
-                       const filteredList = listOfRestaurants.filter((res) => res.data.rating > 4);
+                       const filteredList = listOfRestaurants.filter((res) => res.info.avgRating > 4);
                        setListOfRestaurants(filteredList);
                     }}>Top Rated Restaurants</button>
                 </div>
                 <div className="res-container" >
                 {listOfRestaurants.map((restaurant) => (
-                    <RestaurantCard key={restaurant.data.resid} resData={restaurant} />
+                    <RestaurantCard key={restaurant.info.id} resData={restaurant} />
                 ))}
     
                 </div>
--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import hotel1 from '../../images/hotel1.jpg';
+import { CDN_URL } from '../utils/contants';
 
 function RestaurantCard(props) {
     const {resData} = props;
-    const {name, cuisines, deliveryTime, rating} = resData?.data
+    const {name, cuisines, deliveryTime, avgRating, cloudinaryImageId} = resData?.info
     return(
         <div className="res-card">
-            <img src={hotel1} className="hotel"/>
+            <img src={CDN_URL + cloudinaryImageId} className="hotel"/>
             <h3>{name}</h3>
             <h4>{cuisines.join(",")}</h4>
             <h4>{deliveryTime}</h4>   
-            <h4>{rating}</h4> 
+            <h4>{avgRating}</h4> 
         </div>
     )
 }
